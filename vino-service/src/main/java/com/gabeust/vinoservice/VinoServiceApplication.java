f7468ed51492c5a5ae862504f76faaf96d9389c3
gabeust <romero.gabriel.e@hotmail.com>
2025-04-22 19:37:19 -0300

    creacion vino-service

---
+++ vino-service/src/main/java/com/gabeust/vinoservice/VinoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gabeust.vinoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinoServiceApplication.class, args);
+	}
+
+}
---
+++ vino-service/src/test/java/com/gabeust/vinoservice/VinoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabeust.vinoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
