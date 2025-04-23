62732d223a8b8718db7f940fd887b2fd1b1d08bd
gabeust <romero.gabriel.e@hotmail.com>
2025-04-23 20:31:23 -0300

    add service

--- inventory-service/src/main/java/com/gabeust/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/gabeust/inventoryservice/service/InventoryService.java
@@ -1,4 +1,65 @@
 package com.gabeust.inventoryservice.service;
 
-public class InventoryService {
+import com.gabeust.inventoryservice.entity.Inventory;
+import com.gabeust.inventoryservice.repository.InventoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InventoryService implements InventoryServiceImple{
+
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+    @Override
+    public List<Inventory> findAll() {
+        return inventoryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Inventory> findByWineId(Long wineId) {
+        return inventoryRepository.findByWineId(wineId);
+    }
+
+    @Override
+    public Inventory save(Inventory inventory) {
+        return inventoryRepository.save(inventory);
+    }
+
+    @Override
+    public void increaseStock(Long wineId, int amount) {
+        Inventory inventory = inventoryRepository.findByWineId(wineId)
+                .orElseThrow(() -> new RuntimeException("Inventario no encontrado para el vino con ID: " + wineId));
+
+        inventory.setQuantity(inventory.getQuantity() + amount);
+        inventoryRepository.save(inventory);
+    }
+
+    @Override
+    public void decreaseStock(Long wineId, int amount) {
+        Inventory inventory = inventoryRepository.findByWineId(wineId)
+                .orElseThrow(() -> new RuntimeException("Inventario no encontrado para el vino con ID: " + wineId));
+
+        int newQuantity = inventory.getQuantity() - amount;
+        if (newQuantity < 0) {
+            newQuantity = 0;
+        }
+
+        inventory.setQuantity(newQuantity);
+        inventoryRepository.save(inventory);
+    }
+
+
+    @Override
+    public boolean deleteByWineId(Long wineId) {
+        if (inventoryRepository.existsByWineId()){
+            inventoryRepository.deleteById(wineId);
+            return true;
+        }else return false;
+    }
 }
--- inventory-service/src/main/java/com/gabeust/inventoryservice/service/InventoryServiceImple.java
+++ inventory-service/src/main/java/com/gabeust/inventoryservice/service/InventoryServiceImple.java
@@ -1,4 +1,18 @@
 package com.gabeust.inventoryservice.service;
 
+import com.gabeust.inventoryservice.entity.Inventory;
+import com.gabeust.inventoryservice.repository.InventoryRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface InventoryServiceImple {
+
+    List<Inventory> findAll();
+    Optional<Inventory> findByWineId(Long wineId);
+    Inventory save(Inventory inventory);
+    void increaseStock(Long wineId, int amount);
+    void decreaseStock(Long wineId, int amount);
+    boolean deleteByWineId(Long wineId);
+
 }
