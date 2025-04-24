d04111194b34f14c883193418cec998ba33a8f37
gabeust <romero.gabriel.e@hotmail.com>
2025-04-24 14:20:04 -0300

    update

---
+++ cart-service/src/test/java/com/gabeust/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabeust.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
