d92c57e7697691fc24467e408d12f0e7dce76c12
gabeust <romero.gabriel.e@hotmail.com>
2025-04-24 14:45:50 -0300

    add CartRepository and CartItemRepository

---
+++ cart-service/src/main/java/com/gabeust/cartservice/repository/CartItemRepository.java
@@ -0,0 +1,12 @@
+package com.gabeust.cartservice.repository;
+
+import com.gabeust.cartservice.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByCartId(Long cartId);
+}
