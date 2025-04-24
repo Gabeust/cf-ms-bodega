fd1b34528b6be9e77050388e648ec799ad1352fd
gabeust <romero.gabriel.e@hotmail.com>
2025-04-24 14:34:24 -0300

    create entities Cart and CartItem

---
+++ cart-service/src/main/java/com/gabeust/cartservice/entity/CartItem.java
@@ -0,0 +1,25 @@
+package com.gabeust.cartservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    private Long wineId;
+    private Integer quantity;
+    private Integer priceAtTime;
+}
