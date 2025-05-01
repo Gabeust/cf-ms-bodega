4fff53dfb340bc5bfb1c3c546936be72a1eb3600
gabeust <romero.gabriel.e@hotmail.com>
2025-05-01 10:40:29 -0300

    add entity User and Role

---
+++ user-service/src/main/java/com/gabeust/userservice/entity/User.java
@@ -0,0 +1,33 @@
+package com.gabeust.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private Boolean enable = true;
+    private Boolean accountNotExpired = true;
+    private Boolean accountNotLocked=true;
+    private Boolean credentialNotExpired = true;
+    private int failedAttempts = 0;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    private Set<Role> rolesList = new HashSet<>();
+}
