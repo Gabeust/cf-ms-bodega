1094259d38dc41499d81ab5beeb8af60b0a4405a
gabeust <romero.gabriel.e@hotmail.com>
2025-05-25 10:12:38 -0300

    feat(security): add GatewayAccessFilter to enforce API Gateway access

---
+++ cart-service/src/main/java/com/gabeust/cartservice/config/filter/GatewayAccessFilter.java
@@ -0,0 +1,38 @@
+package com.gabeust.cartservice.config.filter;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class GatewayAccessFilter extends OncePerRequestFilter {
+
+    @Value("${api.gateway.secret}")
+    private String gatewaySecret;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+
+        String header = request.getHeader("X-Gateway-Auth");
+
+        if (header == null || !header.equals(gatewaySecret)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized - must come through API Gateway");
+            return;
+        }
+
+
+        filterChain.doFilter(request, response);
+    }
+
+
+}
